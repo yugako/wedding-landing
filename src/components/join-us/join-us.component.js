e1ae4c54e07c657e476064c7d22ba9d859998e5a
Yurii Hasiuk <Yurii.Hasiuk@externals.adidas.com>
2023-07-24 23:45:51 +0300

    chore: add noreferrer attribute

--- src/components/join-us/join-us.component.js
+++ src/components/join-us/join-us.component.js
@@ -6,7 +6,7 @@ export function JoinUsComponent() {
         <div className="join-us" style={{backgroundImage: `url(${CallToActionImage})`}}>
            <div className="app-container">
                <h2 className="join-us__title">Lets Celebrate Our Love</h2>
-               <a className="join-us__link" href="https://forms.gle/Ph4wMCndR8XtH5RT6" target="_blank">Join with us</a>
+               <a className="join-us__link" href="https://forms.gle/Ph4wMCndR8XtH5RT6" target="_blank" rel='noreferrer'>Join with us</a>
            </div>
         </div>
     )
