409096e7450781e3d2e7de23d112b3e2ab8e6014
Yurii Hasiuk <Yurii.Hasiuk@externals.adidas.com>
2023-07-24 23:21:34 +0300

    feat: add section img title

--- src/components/title/section-title.component.js
+++ src/components/title/section-title.component.js
@@ -5,7 +5,7 @@ export function SectionTitleComponent({sectionTitle}) {
     return (
         <>
             <div className="section-title__img-wrapper">
-                <img src={DoveIcon} className="section-title__img" />
+                <img src={DoveIcon} className="section-title__img" alt={sectionTitle}/>
             </div>
 
             <h2 className="section-title">{sectionTitle}</h2>
