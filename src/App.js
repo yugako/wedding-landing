4ecb23fd9af9cce33e57f85cbff26f234003e37e
Yurii Hasiuk <Yurii.Hasiuk@externals.adidas.com>
2023-07-24 23:44:19 +0300

    feat: add block with google form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.scss';
 import {BannerComponent} from "./components/banner/banner.component";
 import {GalleryComponent} from "./components/gallery/gallery.component";
+import {JoinUsComponent} from "./components/join-us/join-us.component";
 
 function App() {
   return (
-    <main className="app-container">
-        <BannerComponent />
-        <GalleryComponent />
-    </main>
+      <>
+          <main className="app-container">
+              <BannerComponent />
+              <GalleryComponent />
+          </main>
+          <JoinUsComponent />
+      </>
+
   );
 }
 
---
+++ src/components/join-us/join-us.component.js
@@ -0,0 +1,13 @@
+import CallToActionImage from '../../assets/call-to-action.jpeg';
+import './join-us.component.scss';
+
+export function JoinUsComponent() {
+    return (
+        <div className="join-us" style={{backgroundImage: `url(${CallToActionImage})`}}>
+           <div className="app-container">
+               <h2 className="join-us__title">Lets Celebrate Our Love</h2>
+               <a className="join-us__link" href="https://forms.gle/Ph4wMCndR8XtH5RT6" target="_blank">Join with us</a>
+           </div>
+        </div>
+    )
+}
